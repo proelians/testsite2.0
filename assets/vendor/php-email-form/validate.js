fc3e19acccdef73602af1b6974c932d5da01a041
proelians <75449391+proelians@users.noreply.github.com>
2020-12-17 18:52:31 +0100

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -144,16 +144,16 @@
     }).fail( function(data){
       console.log(data);
       var error_msg = "Thank you!<br>";
-//      if(data.statusText || data.status) {
-//        error_msg += 'Status:';
-//       if(data.statusText) {
-//         error_msg += ' ' + data.statusText;
-//        }
-//        if(data.status) {
-//          error_msg += ' ' + data.status;
-//        }
-//        error_msg += '<br>';
-//      }
+      if(data.statusText || data.status) {
+        error_msg += 'Status:';
+       if(data.statusText) {
+         error_msg += ' ' + data.statusText;
+        }
+        if(data.status) {
+          error_msg += ' ' + data.status;
+        }
+        error_msg += '<br>';
+      }
       if(data.responseText) {
         error_msg += data.responseText;
       }
